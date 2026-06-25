d74f71ab75c45dbde233d6a44b27bd5eadfe269e
Amanjot Singh <149322821+amanjot73@users.noreply.github.com>
2026-06-25 23:19:04 +0530

    Learned about the strings

---
+++ Js Part/Js/string.js
@@ -0,0 +1,10 @@
+const name = new String ("aman")
+console.log(`Hello my name is ${name}`);
+console.log(name[0]);
+// console.log(name.indexof('a'))
+console.log(name.toUpperCase());
+console.log(name.indexOf('a'));
+console.log(name.includes('am'));
+
+
+
