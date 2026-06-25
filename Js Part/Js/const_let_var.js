846d5cf52e41b36228842f833dbb04c14bd81acd
Amanjot Singh <149322821+amanjot73@users.noreply.github.com>
2026-06-25 17:40:02 +0530

    Js_01

---
+++ Js Part/Js/const_let_var.js
@@ -0,0 +1,7 @@
+const a = 100;
+let b = 90;
+var c = 10;
+console.table([a,b,c])// method to print multiple things together.....
+
+
+k = "hello" //also a method to declare a variable but not preferred
